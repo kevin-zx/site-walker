31c75a5971df145feebe753b3e93725dcd474327
kevin-zx <zhangxian887@qq.com>
2022-08-11 22:01:30 +0800

    进一步整理基础对象

--- website.go
+++ website.go
@@ -1,54 +1,35 @@
 package sitewalker
 
-type PageType int
+import "net/url"
 
-// 普通网页类型
-const (
-	Home PageType = iota + 1 // 首页
-	About
-	Case
-	Category
-	SubCategory
-	Product
-	Post
-	FAQ
-	TAG
-	Contact
-	Join
-	CustomerService
-	UnKnown
-	Support
-	Download
-	Error
-)
-
-func (p PageType) Name(language int) string {
-	// switch language {
-	// case 1:
-	// todo: complete this
-	return ""
-}
-
-type PageContentType int
-
-// 是列表页还是详情页
-const (
-	List   PageContentType = iota + 1 // 列表页
-	Detail                            // 详情页
-)
-
+// 网站的页面信息
 type Page struct {
-	Title           string
-	Desc            string
-	Keywords        string
-	Link            string
-	H1              string
-	OutPages        []*Page
-	deep            int
-	PageType        PageType
-	PageContentType PageContentType
+	Title         string
+	Desc          string
+	Keywords      []string
+	RawURL        string
+	URL           *url.URL
+	H1            string
+	Pages         map[string]*Page // 这个页面链接到的内页页面
+	ExternalLinks []Href           // 外链
+	Html          []byte           // 网站网页数据
+
+	deep int // 爬取深度
+	// SameDomainExternalLinks []Href // 相同域名的外链 比如 image.baidu.com 页面中 含有 zhidao.baidu.com
 }
 
+// 网站的基本信息
 type WebSite struct {
+	Protocol string
+	Domain   string
 	HomePage *Page
+	Pages    []*Page
+}
+
+// 网站的链接信息
+type Href struct {
+	Href string
+	URL  *url.URL
+	Text string
+	// Title string
 }
